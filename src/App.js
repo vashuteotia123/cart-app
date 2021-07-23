d8467923e026e2279429fb1a1329150a5fe56e1a
Vishal <vashuteotia123@gmail.com>
2021-07-23 20:26:17 +0530

    Added Cart.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import CartItem from "./CartItem";
+import Cart from "./Cart";
 
 function App() {
   return (
     <div className="App">
-      <CartItem />
+      <Cart />
     </div>
   );
 }
