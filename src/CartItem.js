4d6101d7c68d1b8246f32d5b63624d96dc79e1dc
Vishal <vashuteotia123@gmail.com>
2021-07-23 19:35:46 +0530

    added decrease qty

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,8 +10,28 @@ class CartItem extends React.Component {
       img: "",
     };
   }
+  increaseQty = () => {
+    // console.log("this", this.state);
+    // this.setState({
+    //   qty: this.state.qty + 1,
+    // });
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1,
+      };
+    });
+  };
+  decreaseQty = () => {
+    this.setState((prevState) => {
+      if (prevState.qty > 0)
+        return {
+          qty: prevState.qty - 1,
+        };
+    });
+  };
   render() {
     const { price, title, qty } = this.state;
+
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -28,12 +48,14 @@ class CartItem extends React.Component {
               style={styles.icon}
               className="action-icons"
               src="https://image.flaticon.com/icons/png/512/992/992651.png"
+              onClick={this.increaseQty}
             />
             <img
               alt="decrease"
               style={styles.icon}
               className="action-icons"
               src="https://image.flaticon.com/icons/png/512/992/992683.png"
+              onClick={this.decreaseQty}
             />
             <img
               alt="delete"
